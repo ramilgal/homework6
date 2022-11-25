e9b136b0b60fcc386e9b6f97b54d61c419704f36
Ramil <rdgalimov@yandex.ru>
2022-11-26 00:43:04 +0300

    Домашнее задание

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,31 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Домашнее задание");
+        //Задача 1
+        System.out.println("Домашнее задание. Задание 1");
+        for (int i = 1; i <= 10; i++){
+            System.out.println(i);
+        }
+        //Задача 2
+        System.out.println("Задание 2");
+        for (int i = 10; i >= 1; i--){
+            System.out.println(i);
+        }
+        //Задача 3
+        System.out.println("Задание 3");
+        for (int i = 2; i <= 17; i+=2){
+            System.out.println(i);
+        }
+        //Задача 4
+        System.out.println("Задание 4");
+        for (int i = 10; i >= -10; i--){
+            System.out.println(i);
+        }
+        //Задача 4
+        System.out.println("Задание 5");
+        for (int i = 10; i >= -10; i--){
+            System.out.println(i);
+        }
+
 
     }
 }
