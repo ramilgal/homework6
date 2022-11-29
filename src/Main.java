1d9041f796ed2503860e1e38c82d23c0ee9c8c6c
Ramil <rdgalimov@yandex.ru>
2022-11-29 10:10:08 +0300

    Домашнее задание

--- src/Main.java
+++ src/Main.java
@@ -44,7 +44,7 @@ public class Main {
         System.out.println("Домашнее задание 3.Задание 1");
         int input = 29000;
         int total = 0;
-        for (int i = 0; i < 12; i++){
+        for (int i = 1; i <= 12; i++){
             total = total + input;
             System.out.println("Месяц " + i + ", сумма накоплений равна " + total + " рублей");
         }
@@ -52,7 +52,7 @@ public class Main {
         System.out.println("Домашнее задание 3.Задание 2");
         int input1 = 29000;
         int total1 = 0;
-        for (int i = 0; i <= 12; i++){
+        for (int i = 1; i <= 12; i++){
             total1 = total1 + total1/100;
             total1 = total1 + input1;
             System.out.println("Месяц " + i + ", сумма накоплений равна " + total1 + " рублей");
